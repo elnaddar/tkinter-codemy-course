3536023729a9b1006faacfdec7d5c095ec10e21e
Ahmed Mahdy <am.elnaddar@gmail.com>
2025-02-08 14:47:12 +0200

    Add icons and update main app file
    This commit adds various icons (favicon.ico, favicon.xbm, etc.) to the project and updates the main application file (`1_icons_and_close_button.py`) to use the `favicon.xbm` icon.  A `site.webmanifest` file is also added for web app configuration.

---
+++ 3_pretty_advanced_things/1_icons_and_close_button.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+root.iconbitmap('./icons/favicon.xbm')
+# icon = PhotoImage(file='./icons/favicon-32x32.png')
+# root.iconphoto(True, icon)
+
+root.mainloop()
